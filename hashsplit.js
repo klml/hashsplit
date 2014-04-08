dd35b42c03d71efde235cd01c2e10d853e711686
klml <m@klml.de>
2014-04-08 12:48:51 +0200

    unintend all

--- hashsplit.js
+++ hashsplit.js
@@ -1,45 +1,46 @@
-   var HashSearch = new function () {
-       var params;
 
-       this.set = function (key, value) {
-          params[key] = value;
-          this.push();
-       };
+var HashSearch = new function () {
+   var params;
 
-       this.remove = function (key, value) {
-          delete params[key];
-          this.push();
-       };
+   this.set = function (key, value) {
+      params[key] = value;
+      this.push();
+   };
+
+   this.remove = function (key, value) {
+      delete params[key];
+      this.push();
+   };
 
 
-       this.get = function (key, value) {
-           return params[key];
-       };
+   this.get = function (key, value) {
+       return params[key];
+   };
 
-       this.keyExists = function (key) {
-           return params.hasOwnProperty(key);
-       };
+   this.keyExists = function (key) {
+       return params.hasOwnProperty(key);
+   };
 
-       this.push= function () {
-           var hashBuilder = [], key, value;
+   this.push= function () {
+       var hashBuilder = [], key, value;
 
-           for(key in params) if (params.hasOwnProperty(key)) {
-               key = escape(key), value = escape(params[key]); // escape(undefined) == "undefined"
-               hashBuilder.push(key + ( (value !== "undefined") ? '=' + value : "" ));
-           }
+       for(key in params) if (params.hasOwnProperty(key)) {
+           key = escape(key), value = escape(params[key]); // escape(undefined) == "undefined"
+           hashBuilder.push(key + ( (value !== "undefined") ? '=' + value : "" ));
+       }
 
-           window.location.hash = hashBuilder.join("&");
-       };
+       window.location.hash = hashBuilder.join("&");
+   };
 
-       (this.load = function () {
-           params = {}
-           var hashStr = window.location.hash, hashArray, keyVal
-           hashStr = hashStr.substring(1, hashStr.length);
-           hashArray = hashStr.split('&');
+   (this.load = function () {
+       params = {}
+       var hashStr = window.location.hash, hashArray, keyVal
+       hashStr = hashStr.substring(1, hashStr.length);
+       hashArray = hashStr.split('&');
 
-           for(var i = 0; i < hashArray.length; i++) {
-               keyVal = hashArray[i].split('=');
-               params[unescape(keyVal[0])] = (typeof keyVal[1] != "undefined") ? unescape(keyVal[1]) : keyVal[1];
-           }
-       })();
-    }
+       for(var i = 0; i < hashArray.length; i++) {
+           keyVal = hashArray[i].split('=');
+           params[unescape(keyVal[0])] = (typeof keyVal[1] != "undefined") ? unescape(keyVal[1]) : keyVal[1];
+       }
+   })();
+}
