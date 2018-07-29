57006cc89ccc29d342788fdb8ed186a23d0a4653
Klaus Mueller <m@klml.de>
2018-07-29 11:52:45 +0200

    remove tabber, use CSS :target instead

--- hashsplit.js
+++ hashsplit.js
@@ -1,31 +1,5 @@
 function hashsplit() {
 
-    function hashtabber ( htmlId ) {
-
-        var hashtabberfirstactive = 0;
-        var hashtabberclass = 'hashtab'; // TODO configbl
-
-        if (typeof htmlId == "undefined") {
-            var hashtabs = document.getElementsByClassName( hashtabberclass ) ;
-
-            for(var i = 0; i != hashtabs.length; ++i) {
-                hashtabs[i].style.visibility = 'hidden';
-                hashtabs[i].style.position = 'absolute';
-
-                var siblings = hashtabs[i].parentNode.getElementsByClassName( hashtabberclass ) ;
-                siblings[hashtabberfirstactive].style.visibility = 'visible';
-            }
-        } else { // todo not hashtab
-            var siblings = htmlId.parentNode.getElementsByClassName( hashtabberclass ) ;
-
-            for(var i = 0; i != siblings.length; ++i) {
-                siblings[i].style.visibility = 'hidden';
-                siblings[i].style.position = 'absolute';
-            }
-            htmlId.style.visibility = 'visible'; // TODO not hashtab
-        }
-    };
-
     function hashForm ( htmlId, hashVal ) {
         if( htmlId.getAttribute('type') == 'checkbox' ) {
             // check checkbox on 'true' or any string, but uncheck with 'false' and '' (empty string) only
@@ -42,10 +16,6 @@ function hashsplit() {
         if (htmlId == null )  return  ;
 
         switch( htmlId.tagName ) {
-            case 'P' :
-            case 'DIV' :
-                hashtabber( htmlId );
-            break
             case 'INPUT' :
             case 'SELECT' :
                 hashForm( htmlId, hashVal );
